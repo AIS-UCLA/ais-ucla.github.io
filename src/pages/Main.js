0ea0354763735394aaf7958086d16732b848ddfb
MariusPL <mario.penglee.mpl@gmail.com>
2022-07-26 16:50:26 -0700

    one page nav

---
+++ src/pages/Main.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Home, About, Join, Projects } from './';
+
+const Main = () => (
+	<main>
+		<Home />
+		<About />
+		<Projects />
+		<Join />
+	</main>
+);
+
+export default Main;
---
+++ src/pages/index.js
@@ -0,0 +1,5 @@
+export { default as About } from './About';
+export { default as Join } from './Join';
+export { default as Home } from './Home';
+export { default as Main } from './Main';
+export { default as Projects } from './Projects';
