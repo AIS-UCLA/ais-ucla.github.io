09b05a520aba17c725d3a9713699b770aff23883
Tejas Kamtam <tejaskamtam@yahoo.com>
2022-09-17 11:22:47 -0700

    body edits

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,37 +1,58 @@
 import { ThreeDotsVertical } from "react-bootstrap-icons";
 
-import Logo from "../assets/banner.png";
+import Banner from "../assets/banner.png";
+import Logo from "../assets/logo.png";
 
 const ProjectsUrl = "#projects";
 const IntroProgramUrl = "#intro";
 const WorkshopsUrl = "#workshops";
+const JoinUrl = '/forms/intro';
 
-const Header = () =>
-  <header className="pb-5">
-    <nav className="navbar navbar-expand-lg py-3 fixed-top bg-white">
-      <div className="container d-flex">
-        <a className="navbar-brand" href="/">
-
-
-          <h4><b>AI Safety</b></h4>
-        </a>
-        {/*Collapsible Button*/}
-        <button type="button" className="navbar-toggler"
-          data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"
-        >
-          <ThreeDotsVertical className="text-body" />
-        </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          {/*Right Aligned*/}
-          <div className="navbar-nav ms-auto">
-            <a href={ProjectsUrl} className="nav-item nav-link"><strong>Projects</strong></a>
-            <a href={IntroProgramUrl} className="nav-item nav-link"><strong>Intro to AI Safety Program</strong></a>
-            <a href={WorkshopsUrl} className="nav-item nav-link"><strong>Workshops</strong></a>
-          </div>
-        </div>
-      </div>
-    </nav>
-  </header>
+const Header = () => (
+    <header className="pb-5">
+        <nav className="navbar navbar-expand-lg py-3 fixed-top bg-white">
+            <div className="container d-flex">
+                <a href="/"><img src={Logo} style={{maxWidth: "3rem"}}></img></a>
+                <a className="navbar-brand justify-content-start" href="/">
+                    <h4>
+                        <b>AI Safety</b>
+                    </h4>
+                </a>
+                {/*Collapsible Button*/}
+                <button
+                    type="button"
+                    className="navbar-toggler"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent"
+                    aria-controls="navbarSupportedContent"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                >
+                    <ThreeDotsVertical className="text-body" />
+                </button>
+                <div
+                    className="collapse navbar-collapse"
+                    id="navbarSupportedContent"
+                >
+                    {/*Right Aligned*/}
+                    <div className="navbar-nav ms-auto">
+                        <a href={IntroProgramUrl} className="nav-item nav-link">
+                            <strong>Intro to AI Safety Program</strong>
+                        </a>
+                        <a href={WorkshopsUrl} className="nav-item nav-link">
+                            <strong>Workshops</strong>
+                        </a>
+                        <a href={ProjectsUrl} className="nav-item nav-link">
+                            <strong>Projects</strong>
+                        </a>
+                        <a href={JoinUrl} className="nav-item nav-link">
+                            <strong>Join</strong>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    </header>
+);
 
 export default Header;
--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -3,70 +3,71 @@ import Layout from "../components/Layout";
 import Section from "../components/Section";
 
 import { CaretDownFill } from "react-bootstrap-icons";
-import Logo from "../assets/banner.png";
+import Banner from "../assets/banner.png";
 
 const Home = () => (
-  <Layout>
-    <div id="content" className="container">
-      <Section id="home" name="">
-        <div className="d-inline-flex justify-content-start flex-wrap p-2">
-          <img
-            className="d-inline-flex justify-content-end"
-            src={Logo}
-            style={{
-              maxHeight: "15rem",
-              maxWidth: "15rem",
-            }}
-          ></img>
-          <div id="slogan" className="my-2">
-            <p>
-              AI Safety at UCLA researches the impact of
-              increasingly powerful AI systems. We promote the
-              development of fair, secure, and useful AI tools.
-              Join us if you'd like to learn about AI Safety and
-              research how we can use AI to build a better future.
-            </p>
-          </div>
-          <div
-            id="quote"
-            className="border border-dark border-2 my-2"
-          >
-            <p className="px-2 pt-3">
-              Better AI Safe than AI Sorry
-            </p>
-          </div>
+    <Layout>
+        <div id="content" className="container">
+            <Section id="home" name="">
+                <div className="d-flex justify-content-start flex-wrap p-2">
+                    <img
+                        className="d-inline-flex justify-content-end"
+                        src={Banner}
+                        style={{
+                            maxHeight: "15rem",
+                            maxWidth: "15rem",
+                        }}
+                    ></img>
+                    <div id="slogan" className="my-2">
+                        <p>
+                            AI Safety at UCLA researches the impact of
+                            increasingly powerful AI systems. We promote the
+                            development of fair, secure, and useful AI tools.
+                            Join us if you'd like to learn about AI Safety and
+                            research how we can use AI to build a better future.
+                        </p>
+                    </div>
+                    <div
+                        id="quote"
+                        className="border border-dark border-2 my-2"
+                    >
+                        <p className="px-2 pt-3">
+                            "Better AI Safe than AI Sorry"
+                        </p>
+                    </div>
+                </div>
+            </Section>
+            <Section id="fellowship" name="">
+                <div>
+                    <h2 className="py-2">Intro to AI Safety Program</h2>
+                    <h4><u>About</u></h4>
+                    <h4><u>Eligibility</u></h4>
+                    <h4><u>Apply</u></h4>
+                </div>
+            </Section>
+            <Section id="workshops" name="">
+                <div>
+                    <h2 className="py-2">Workshops</h2>
+                    <h4><u>About</u></h4>
+                    <h4><u>Event Timings</u></h4>
+                    <h4><u>RSVP</u></h4>
+                </div>
+            </Section>
+            <Section id="projects" name="">
+                <div>
+                    <h2 className="py-2">Projects</h2>
+                    <h4><u>About</u></h4>
+                    <h4><u>Eligibility</u></h4>
+                    <h4><u>Apply</u></h4>
+                </div>
+            </Section>
+            <Section id="join" name="">
+                <div>
+                    <a href="/forms/intro"><h2 className="py-2">Join</h2></a>
+                </div>
+            </Section>
         </div>
-      </Section>
-      <Section id="fellowship" name="">
-      <div style={{backgroundColor: "RED"}}>
-        <p>hi</p>
-        <p>hi</p>
-        <p>hi</p>
-        <p>hi</p>
-        <p>hi</p>
-        <p>hi</p>
-        <p>hi</p>
-        <p>hi</p>
-        <p>hi</p>
-        <p>hi</p>
-        <p>hi</p>
-        <p>hi</p>
-        <p>hi</p>
-        <p>hi</p>
-        <p>hi</p>
-        <p>hi</p>
-        <p>hi</p>
-        <p>hi</p>
-        <p>hi</p>
-        <p>hi</p>
-        <p>hi</p>
-        <p>hi</p>
-        <p>hi</p>
-        <p>hi</p>
-        </div>
-      </Section>
-    </div>
-  </Layout>
+    </Layout>
 );
 
 import React from "react";
@@ -78,9 +79,9 @@ import ReactDOM from "react-dom";
 import "bootstrap/dist/js/bootstrap";
 import "../styles/style.scss";
 
-const root = createRoot(document.getElementById('root')!);
+const root = createRoot(document.getElementById("root")!);
 root.render(
-  <React.StrictMode>
-    <Home />
-  </React.StrictMode>,
+    <React.StrictMode>
+        <Home />
+    </React.StrictMode>
 );
